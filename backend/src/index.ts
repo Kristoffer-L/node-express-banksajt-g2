f353f7663a2e6ff290cfbc0d92618d4ab95e1ad0
Elin Suvinen <elin1.suvinen@gmail.com>
2025-02-27 20:12:24 +0100

    backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,25 +2,24 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
-const port = 3000
+const port = 3000;
 interface User {
-  id: number| string,
-  username: string,
-  password: string
+  id: number | string;
+  username: string;
+  password: string;
 }
 
 interface Account {
-  id: number| string,
-  user_id: User["id"]
-  amount: number
+  id: number | string;
+  user_id: User["id"];
+  balance: number;
 }
 
 interface Session {
-  id: number| string,
-  user_id: User["id"]
-  token: number
+  id: number | string;
+  user_id: User["id"];
+  token: number | string;
 }
-
 
 // Middleware
 app.use(cors());
@@ -28,38 +27,60 @@ app.use(express.json());
 
 // Generera engångslösenord
 function generateOTP() {
-    // Generera en sexsiffrig numerisk OTP
-    const otp = Math.floor(100000 + Math.random() * 900000);
-    return otp.toString();
+  // Generera en sexsiffrig numerisk OTP
+  const otp = Math.floor(100000 + Math.random() * 900000);
+  return otp.toString();
 }
 
 // Din kod här. Skriv dina arrayer
 
-const users: User[] = [{ id:1, username:"kristoffer", password:"password"}]
-const accounts: Account[]  = [{id:1, user_id: 1, amount:100}]
-const sessions: Session[] = [{id:1, user_id: 1, token:123}]
+const users: User[] = [
+  { id: 101, username: "kristoffer", password: "password" },
+];
+const accounts: Account[] = [{ id: 1, user_id: 101, balance: 100 }];
+const sessions: Session[] = [{ id: 1, user_id: 101, token: "" }];
 
 // Din kod här. Skriv dina routes:
 
 //Skapa användare (POST): "/users"
 app.post("/users", (req, res) => {
-  res.json(req.body);
+  res.json(req.body.username);
 });
 //Logga in (POST): "/sessions"
 app.post("/sessions", (req, res) => {
-  res.json(req.body)
+  users.forEach((user) => {
+    if (
+      req.body.username === user.username &&
+      req.body.password === user.password
+    ) {
+      const newSession: Session = {
+        id: sessions.length + 1,
+        user_id: user.id,
+        token: generateOTP(),
+      };
+      sessions.push(newSession);
+      res.json({
+        message: "Inloggning lyckades",
+        token: newSession.token,
+      });
+      return;
+    }
+  });
+  res.status(401).json({
+    message: "Fel användarnamn eller lösenord",
+  });
 });
+
 //Visa salodo (POST): "/me/accounts"
 app.post("/me/accounts", (req, res) => {
-  res.send("users here");
+  // res.json({ balance });
 });
 //Sätt in pengar (POST): "/me/accounts/transactions"
 app.post("/me/accounts/transactions", (req, res) => {
   res.send("users here");
 });
 
-
 // Starta servern
 app.listen(port, () => {
-    console.log(`Bankens backend körs på http://localhost:${port}`);
+  console.log(`Bankens backend körs på http://localhost:${port}`);
 });
